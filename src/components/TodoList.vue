e0f5ea9db842eb218c533bc506c476e458340a44
Luis Antonio <luis.antonio.c.of@gmail.com>
2024-03-17 13:55:26 +0000

    add task functionality

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="container">
-    <div class="test"></div>
     <label class="form-label">TODO List</label>
     <form>
       <input
         type="text"
         placeholder="example of a task"
+        v-model="store.newItem"
+        @keyup.enter.prevent="store.addItem"
         class="todo-input"
-        v-model="task"
-        @keyup.enter="addTodo"
       />
-      <button class="todo-button" @click="addTodo">Add Task</button>
+      <button @click.prevent="store.addItem" class="todo-button">
+        Add Task
+      </button>
     </form>
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+    <div v-for="todo in store.items" :key="todo.id" class="todo-item">
       {{ todo.title }}
     </div>
   </div>
+  <br />
+  <br />
+  <br />
+  <label for="test"> {{ store.newItem }}</label>
 </template>
 
-<script></script>
-
-<style scoped></style>
+<script setup>
+import { useTaskStore } from "@/stores/taskStore";
+const store = useTaskStore();
+</script>
