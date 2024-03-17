05ece0fd64c17186a5b4eee61ffe2fa8cc103da0
Luis Antonio <luis.antonio.c.of@gmail.com>
2024-03-17 03:21:44 +0000

    add base template for to do app

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="test"></div>
+    <label class="form-label">TODO List</label>
+    <form>
+      <input
+        type="text"
+        placeholder="example of a task"
+        class="todo-input"
+        v-model="task"
+        @keyup.enter="addTodo"
+      />
+      <button class="todo-button" @click="addTodo">Add Task</button>
+    </form>
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
